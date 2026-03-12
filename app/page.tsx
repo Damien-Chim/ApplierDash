13714ed796de49e0a6d93c71aa7e649ac2eab8ec
Damien <damienchimsengyuan@gmail.com>
2026-03-12 21:08:49 +1100

    Edited GET request code for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,15 @@ export default function Home() {
   useEffect(() => {
     async function getApplications() {
       try {
-        const response = await fetch("http://127.0.0.1:8000/applications");
+        const response = await fetch(
+          "http://127.0.0.1:8000/applications",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json"
+            }
+          }
+        );
         if (!response.ok) throw new Error("Failed to fetch data");
 
         const data = await response.json();
